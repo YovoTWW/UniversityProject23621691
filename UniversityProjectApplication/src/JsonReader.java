0bb8734473fa411791f16ddd765b2f86cfd28b8f
YovoTWW <yovo352@gmail.com>
2025-04-16 18:52:21 +0300

    Добавена първоначална логика за валидиране

--- UniversityProjectApplication/src/JsonReader.java
+++ UniversityProjectApplication/src/JsonReader.java
@@ -8,14 +8,18 @@ public class JsonReader {
     public static void main(String[] args) {
         {
             try {
-                String content = new String(Files.readAllBytes(Paths.get("src/JSON_Files/data.json")));
+                String content1 = new String(Files.readAllBytes(Paths.get("src/JSON_Files/data.json")));
+                String content2 = new String(Files.readAllBytes(Paths.get("src/JSON_Files/invalid.json")));
 
-                Map<String, String> jsonMap = parseJson(content);
-                
-                for (String key : jsonMap.keySet()) {
-                    String value = jsonMap.get(key);
-                    System.out.println(key + ": "+value);
-                }
+                Map<String, String> jsonMap = parseJson(content2);
+
+                    for (String key : jsonMap.keySet()) {
+                        String value = jsonMap.get(key);
+                        System.out.println(key + ": "+value);
+                    }
+
+                validateJson(content2);
+
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -42,7 +46,23 @@ public class JsonReader {
                 map.put(keyValue[0].trim(), keyValue[1].trim());
             }
         }
-
         return map;
     }
+
+    public static void validateJson(String json){
+
+        json = json.trim().replaceAll("[{}\"]", "");
+        String[] pairs = json.split(",");
+
+
+        for (String pair : pairs) {
+            String[] keyValue = pair.split(":");
+            if (keyValue.length != 2) {
+               System.out.println("Invalid Json format at row : "+pair);
+            }
+
+        }
+    }
+
+
 }
